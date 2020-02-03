df9c3711b3d268f9125acc25e13c9201590f1a0b
jprados <jesuspradosp@gmail.com>
2020-02-03 14:04:10 +0100

    Third commit

--- src/software1.java
+++ src/software1.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class software1 {
@@ -5,23 +6,25 @@ public class software1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Introduzca el nombre del estudiante:");
-		String nombre = sc.nextLine();
-		System.out.println("Introduzca los apellidos:");
-		String apellido = sc.nextLine();
-		System.out.println("Fecha de nacimiento:");
-		String fecha_nacimiento = sc.nextLine();
-		System.out.println("DNI:");
-		String dni = sc.nextLine();
-		System.out.println("Estudios previos:");
-		String estudios = sc.nextLine();
-		System.out.println("telefono:");
-		String telefono = sc.nextLine();
-		System.out.println("Curso actual");
-		String curso_actual = sc.nextLine();
+		ArrayList<String> datos = new ArrayList<String>();
 		
-		System.out.println("El estudiante :" + nombre +" " + apellido + " nacido el : " + fecha_nacimiento + 
-							" con dni: " + dni + " estudiando " + curso_actual + " con telefono: " + telefono);
+		System.out.println("Introduzca el nombre del estudiante:");
+		datos.add(sc.nextLine());
+		System.out.println("Introduzca los apellidos:");
+		datos.add(sc.nextLine());
+		System.out.println("Fecha de nacimiento:");
+		datos.add(sc.nextLine());
+		System.out.println("DNI:");
+		datos.add(sc.nextLine());
+		System.out.println("Estudios previos:");
+		datos.add(sc.nextLine());
+		System.out.println("telefono:");
+		datos.add(sc.nextLine());
+		System.out.println("Curso actual");
+		datos.add(sc.nextLine());
+		
+		System.out.println("El estudiante :" + datos.get(0) +" " + datos.get(1) + " nacido el : " + datos.get(2) + 
+							" con dni: " + datos.get(3) + " estudiando " + datos.get(6) + " con telefono: " + datos.get(5));
 	
 	
 	}
